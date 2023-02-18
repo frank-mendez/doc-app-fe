29772aff0f91e09c9b46c42c2298ba15e9d52709
frankmendez <frankmendezresources@gmail.com>
2023-02-19 01:41:08 +0800

    Added mongodb on backend, updated reactjs on client

---
+++ backend/src/database.providers.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> => {
+      console.log('process.env.MONGODB_URL', process.env.MONGODB_URL);
+      const connection = mongoose.connect(process.env.MONGODB_URL);
+      connection.then((result) => {
+        console.log('result', result);
+      });
+      console.log('connection', connection);
+      return connection;
+    },
+  },
+];
