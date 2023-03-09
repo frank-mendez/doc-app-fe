ec7cc2a35add300bf1c724b365050c58eda37563
frankmendez <frankmendezresources@gmail.com>
2023-03-09 16:17:00 +0800

    Frontend: Added sidebar collapsed feature

--- client/src/Common/Layout/CommonLayout.tsx
+++ client/src/Common/Layout/CommonLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { userMenu } from '../Menu/Menu'
 import { LayoutProps } from '../types'
@@ -6,10 +6,14 @@ import './CommonLayout.style.scss'
 
 const CommonLayout = ({ children }: LayoutProps) => {
 	const location = useLocation()
+	const [collapsed, setCollapsed] = useState<boolean>(false)
+	const collapseHandler = () => {
+		setCollapsed(!collapsed)
+	}
 	return (
 		<div className='main p-2'>
 			<div className='layout d-flex'>
-				<div className='sidebar'>
+				<div className={collapsed ? 'collapse-sidebar' : 'sidebar'}>
 					<div className='sidebar-header'>
 						<h1>App</h1>
 					</div>
@@ -25,7 +29,13 @@ const CommonLayout = ({ children }: LayoutProps) => {
 					</div>
 				</div>
 				<div className='content'>
-					<div className='header'>Header</div>
+					<div className='header'>
+						{!collapsed ? (
+							<i onClick={collapseHandler} className='ri-menu-fold-line header-icon'></i>
+						) : (
+							<i onClick={collapseHandler} className='ri-menu-unfold-line header-icon'></i>
+						)}
+					</div>
 					<div className='body'>{children}</div>
 				</div>
 			</div>
